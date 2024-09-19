d98f952b0a26fde6f6c45085023e2169e2a30f6e
DanielSw73 <sand777333@gmail.com>
2024-09-19 20:47:59 -0300

    feat: Tarea de Clases implementada - Sprint 2

--- js/producto.js
+++ js/producto.js
@@ -6,27 +6,23 @@ class Producto {
         this.stock = stock;
         this.imagen = imagen;
     }
-
-    mostrarProducto() {
-        const container = document.querySelector(".container-product");
-        container.innerHTML += `
-        <div class="card col-12 col-sm-6 col-md-4 col-lg-3 p-0" style="width: 18rem;">
-                <img src="${url}/${this.imagen}.jpg" class="card-img-top w-100" style="height: 13rem; object-fit: cover; object-position: center" alt="${this.imagen}.jpg">
-                <div class="card-body text-center">
-                    <h5 class="card-title">${this.titulo}</h5>
-                    <p class="card-text">${this.detalle}</p>
-                    <p class="card-text fw-bold">Stock: ${this.stock}</p>
-                    <p class="card-text fw-bold">Precio: $${this.precio}</p>
-                </div>
-            </div>
-        `;
-    }
 }
 
 const url = "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app";
+const container = document.querySelector(".container-product");
 
 const product1 = new Producto("Renault", "Some quick example text to build on the card title and make up the bulk of the card's content.", 3423, 12, 2);
-const product2 = new Producto("Renault2", "Some quick example text to build on the card title and make up the bulk of the card's content.", 3423, 12, 8);
 
-product1.mostrarProducto();
-product2.mostrarProducto();
+const etiquetas = `
+    <div class="card col-12 col-sm-6 col-md-4 col-lg-3 p-0" style="width: 18rem;">
+        <img src="${url}/${product1.imagen}.jpg" class="card-img-top w-100" style="height: 13rem; object-fit: cover; object-position: center" alt="${product1.imagen}.jpg">
+        <div class="card-body text-center">
+            <h5 class="card-title">${product1.titulo}</h5>
+            <p class="card-text">${product1.detalle}</p>
+            <p class="card-text fw-bold">Stock: ${product1.stock}</p>
+            <p class="card-text fw-bold">Precio: $${product1.precio}</p>
+        </div>
+    </div>
+`;
+
+container.innerHTML = etiquetas;
